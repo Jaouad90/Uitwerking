146484f50eff8a8fd9ca9e1eadf3679dc349d605
Jaouad90 <kaouch.j@gmail.com>
2021-05-12 06:33:33 +0200

    tiny fix! and todo: make full dataset for tripadvice button options available

--- resources/views/parts/specifictripadvice.blade.php
+++ resources/views/parts/specifictripadvice.blade.php
@@ -1,51 +1,49 @@
 @if(!empty($tripDataJSON))
-    <div class="container mt-5 mb-5">
-        <div class="row">
-            <div class="col-md-6 offset-md-3">
+<div class="container mt-5 mb-5">
+    <div class="row">
+        <div class="col-md-6 offset-md-3">
 
-                <div class="time-choice-container">
-                    <button class="left-button"><</button>
-                    <button class="right-button">></button>
-                        @foreach ($tripDataJSON['trips'] as $tripData)
-                            @php 
-                            foreach ($tripData['legs'] as $tripLegsData)
-                            {
-                                $index = strval($loop->index);
-                                $originActualDateTime = date("H:i",strtotime($tripLegsData['origin']['actualDateTime']));
-                                $destinationActualDateTime = date("H:i",strtotime($tripLegsData['destination']['actualDateTime']));
-                            }
-                            @endphp
-                            <script>    
-                                var tripLegsData = {!! json_encode($tripLegsData) !!};
-                            </script>
-                                <div class="button-container">
-                                    <button class="button-trip-choice btn btn-primary" onclick="showTripAdvice(tripLegsData)" type="button">
-                                    {{$originActualDateTime .' -> '. $destinationActualDateTime}}
-                                    </button>
-                                </div>
-                        @endforeach
-                </div>
+            <div class="time-choice-container">
+                <button class="left-button"><</button>
+                <button class="right-button">></button>
+                @foreach ($tripDataJSON['trips'] as $tripData)
+                    @foreach ($tripData['legs'] as $tripLegsData)
+                    {
+                        $index = strval($loop->index);
+                        $originActualDateTime = date("H:i",strtotime($tripLegsData['origin']['plannedDateTime']));
+                        $destinationActualDateTime = date("H:i",strtotime($tripLegsData['destination']['plannedDateTime']));
+                    }
+                    <!-- TODO: The latest is shown now. but the goal is to have the full dataset en search based on time -->
+                <script>
+                    var tripLegsData = {!!json_encode($tripLegsData) !!};
+                </script>
+                    <div class="button-container">
+                        <button class="button-trip-choice btn btn-primary" onclick="showTripAdvice(tripLegsData)" type="button">
+                            {{$originActualDateTime .' -> '. $destinationActualDateTime}}
+                        </button>
+                    </div>
+                @endforeach
+            </div>
 
-                
-                <ul class="timeline" style="visibility: hidden;">
-                    <div class="tripadvice-departure">
+
+            <ul class="timeline" style="visibility: hidden;">
+                <div class="tripadvice-departure">
+                    <li>
                         <div class="time-position planned-departure-time"></div>
                         <div class="planned-departure-track">Vertrek: Spoor </div>
-                        <a class="planned-departure-station" target="_blank" href="">
-                            Station </a>
-                    </div>
-                    <div class="tripadvice-arrival">
+                        <a class="planned-departure-station" target="_blank" href="">Station </a>
+                    </li>
+                </div>
+                <div class="tripadvice-arrival">
+                    <li>
                         <div class="time-position planned-arrival-time"></div>
                         <div class="planned-arrival-track">Vertrek: Spoor </div>
-                        <a class="planned-arrival-station" target="_blank" href="">
-                            Station </a>
-                    </div>
-                </ul>
+                        <a class="planned-arrival-station" target="_blank" href="">Station </a>
+                    </li>
+                </div>
+            </ul>
 
-            </div>
         </div>
     </div>
+</div>
 @endif
-
-
-
