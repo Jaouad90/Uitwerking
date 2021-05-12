cc376b01e178edc6b9e5c9958097714d1c4bb76d
Jaouad90 <kaouch.j@gmail.com>
2021-05-12 03:32:43 +0200

    initial tripAdvice

--- public/js/all.js
+++ public/js/all.js
@@ -172,3 +172,61 @@ const DEFAULTS = {
   }
 
 ac.setData(setData4AutoCompletion());
+
+var showTripAdvice = function (tripData)
+{
+    // Container visible because a choice has been made.
+    var containerVisibility = elementFinder('.timeline');
+    containerVisibility.style.visibility = 'visible';
+
+/* Start of departure section */
+    // Get departure elements.
+    var departureTimeElem = elementFinder('.planned-departure-time');
+    var departureTrackElem = elementFinder('.planned-departure-track');
+    var departureStationElem = elementFinder('.planned-departure-station');
+
+    // Get time from datetime string
+    var departureDateTime = tripData.origin.actualDateTime;
+    var startPosition = departureDateTime.search("T") +1;
+    var endPosition = startPosition + 5;
+    departureTime = departureDateTime.substring(""+startPosition, ""+endPosition);
+
+    // Show results to the user
+    var content = document.createTextNode(departureTime);
+    departureTimeElem.appendChild(content);
+    content = document.createTextNode(tripData.origin.actualTrack);
+    departureTrackElem.appendChild(content);
+    content = document.createTextNode(tripData.origin.name);
+    departureStationElem.appendChild(content);
+/* End of departure section */
+
+/* Start of arrival section */
+    // Get arrival elements.
+    var arrivalTimeElem = elementFinder('.planned-arrival-time');
+    var arrivalTrackElem = elementFinder('.planned-arrival-track');
+    var arrivalStationElem = elementFinder('.planned-arrival-station');
+
+    // Get time from datetime string
+    var arrivalDateTime = tripData.destination.actualDateTime;
+    var startPosition = arrivalDateTime.search("T") +1;
+    var endPosition = startPosition + 5;
+    arrivalTime = arrivalDateTime.substring(""+startPosition, ""+endPosition);
+
+    // Show results to the user
+    var content = document.createTextNode(arrivalTime);
+    arrivalTimeElem.appendChild(content);
+    content = document.createTextNode(tripData.destination.actualTrack);
+    arrivalTrackElem.appendChild(content);
+    content = document.createTextNode(tripData.destination.name);
+    arrivalStationElem.appendChild(content);
+/* End of arrival section */
+    
+
+
+    console.log(tripData);
+}
+
+var elementFinder = function (selector) {
+
+    return document.querySelector(selector);
+};
--- resources/views/parts/specifictripadvice.blade.php
+++ resources/views/parts/specifictripadvice.blade.php
@@ -11,13 +11,16 @@
                             foreach ($tripData['legs'] as $tripLegsData)
                             {
                                 $index = strval($loop->index);
-                                $originPlannedDateTime = date("H:i",strtotime($tripLegsData['origin']['plannedDateTime']));
-                                $destinationPlannedDateTime = date("H:i",strtotime($tripLegsData['destination']['plannedDateTime']));
+                                $originActualDateTime = date("H:i",strtotime($tripLegsData['origin']['actualDateTime']));
+                                $destinationActualDateTime = date("H:i",strtotime($tripLegsData['destination']['actualDateTime']));
                             }
                             @endphp
+                            <script>    
+                                var tripLegsData = {!! json_encode($tripLegsData) !!};
+                            </script>
                                 <div class="button-container">
-                                    <button class="button-trip-choice btn btn-primary" onclick="showTripAdvice($tripLegsData)" type="button">
-                                    {{$originPlannedDateTime .' -> '. $destinationPlannedDateTime}}
+                                    <button class="button-trip-choice btn btn-primary" onclick="showTripAdvice(tripLegsData)" type="button">
+                                    {{$originActualDateTime .' -> '. $destinationActualDateTime}}
                                     </button>
                                 </div>
                         @endforeach
@@ -34,7 +37,7 @@
                     <div class="tripadvice-arrival">
                         <div class="time-position planned-arrival-time"></div>
                         <div class="planned-arrival-track">Vertrek: Spoor </div>
-                        <a class="planned-departure-station" target="_blank" href="">
+                        <a class="planned-arrival-station" target="_blank" href="">
                             Station </a>
                     </div>
                 </ul>
